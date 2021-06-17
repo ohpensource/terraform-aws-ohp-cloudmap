a24c43bdb5773455927a2f4a5b7fac60fec31231
Rupert <rbroad@ohpen.com>
2021-06-17 14:29:01 +0200

    fix namespace name

--- modules/http_namespace/locals.tf
+++ modules/http_namespace/locals.tf
@@ -1,3 +1,2 @@
 locals {
-  namespace_name = "${var.client}${var.envs[count.index]}-${var.platform}"
 }
--- modules/http_namespace/main.tf
+++ modules/http_namespace/main.tf
@@ -1,11 +1,11 @@
 resource "aws_service_discovery_http_namespace" "main" {
   count       = length(var.envs)
-  name        = lower(local.namespace_name)
+  name        = lower("${var.client}${var.envs[count.index]}-${var.platform}")
   description = "Namespace ${upper(var.client)} ${upper(var.envs[count.index])} ${upper(var.platform)}"
 
   tags = merge(var.tags,
     tomap({
-      "Name" = local.namespace_name
+      "Name" = "${var.client}${var.envs[count.index]}-${var.platform}"
   }))
 }
 
